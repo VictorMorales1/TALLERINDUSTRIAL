2101549a5a21428cf6c0b49acc000ca6cb7b4d71
VictorMorales1 <120761348+VictorMorales1@users.noreply.github.com>
2022-12-16 12:46:58 -1200

    Add files via upload

---
+++ CapaNegocio/CN_Proveedor.cs
@@ -0,0 +1,81 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+namespace CapaNegocio
+{
+    public class CN_Proveedor
+    {
+        private CD_proveedor objcd_Proveedor = new CD_proveedor();
+
+        public List<Proveedor> Listar()
+        {
+            return objcd_Proveedor.Listar();
+        }
+
+        public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.RazonSocial == "")
+            {
+                Mensaje += "Es necesario la Razon Social del Proveedor\n";
+            }
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el Documento del Proveedor\n";
+            }
+            if (obj.Correo == "")
+            {
+                Mensaje += "Es necesaria la Correo del Proveedor\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objcd_Proveedor.Registrar(obj, out Mensaje);
+            }
+        }
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.RazonSocial == "")
+            {
+                Mensaje += "Es necesario la Razon Social del Proveedor\n";
+            }
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el Documento del Proveedor\n";
+            }
+            if (obj.Correo == "")
+            {
+                Mensaje += "Es necesaria la Correo del Proveedor\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return objcd_Proveedor.Editar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
+        {
+            return objcd_Proveedor.Eliminar(obj, out Mensaje);
+        }
+    }
+}
---
+++ CapaNegocio/CN_Usuario.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+using CapaNegocio;
+
+namespace CapaNegocio
+{
+    public class CN_Usuario
+    {
+        private CD_Usuario objcd_usuario = new CD_Usuario();
+
+        public List<Usuario> Listar()
+        {
+            return objcd_usuario.Listar();
+        }
+
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.NombreCompleto == "")
+            {
+                Mensaje += "Es necesario el Nombre Completo\n";
+            }
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el Documento\n";
+            }
+            if (obj.Clave == "")
+            {
+                Mensaje += "Es necesaria la Clave\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objcd_usuario.Registrar(obj, out Mensaje);
+            } 
+        }
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.NombreCompleto == "")
+            {
+                Mensaje += "Es necesario el Nombre Completo\n";
+            }
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el Documento\n";
+            }
+            if (obj.Clave == "")
+            {
+                Mensaje += "Es necesaria la Clave\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return objcd_usuario.Editar(obj, out Mensaje);
+            }
+            
+        }
+
+        public bool Eliminar(Usuario obj, out string Mensaje)
+        {
+            return objcd_usuario.Eliminar(obj, out Mensaje);
+        }
+    }
+}
